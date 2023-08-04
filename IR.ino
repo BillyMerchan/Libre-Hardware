8ebdb28926534f0a89acf8d07d164ba3a600d1f7
Billy Merchan <billymerchan@gmail.com>
2023-08-03 21:48:06 -0400

    Messing around with arduino

---
+++ IR.ino
@@ -0,0 +1,14 @@
+void setup(){
+    pinMode(13, OUTPUT);
+    pinMode(3, INPUT);
+    Serial.begin(9600);
+}
+void loop(){
+    if(digitialRead(3) == LOW){
+	digitalWrite(13, HIGH);
+	delay(10);
+    } else {
+	digitalWrite(13, LOW);
+	delay(10);
+    }
+}
